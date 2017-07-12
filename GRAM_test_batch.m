ae2648628571cee20e4307e7ad1dcdbdacb845b6
abhineet123 <asingh1@ualberta.ca>
2017-07-11 22:13:58 -0600

    created GRAM_test_batch;

---
+++ GRAM_test_batch.m
@@ -0,0 +1,14 @@
+GRAM_test;
+clear all;
+cd ../MDP_Tracking_2
+GRAM_test;
+clear all;
+cd ../MDP_Tracking_3
+GRAM_test;
+clear all;
+cd ../MDP_Tracking_4
+GRAM_test;
+clear all;
+cd ../MDP_Tracking_5
+GRAM_test;
+clear all;
