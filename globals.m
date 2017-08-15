1a79a758cb0cf6bf8acc9d2591a7513542a35c74
abhineet123 <asingh1@ualberta.ca>
2017-08-15 16:31:27 -0600

    minor fix

--- globals.m
+++ globals.m
@@ -8,13 +8,13 @@ function opt = globals()
 
 %% Important settings
 
-gram_train_ratio = 1;
+gram_train_ratio = 0.1;
 gram_split_train_ratio = 0.6;
 idot_train_ratio = -0.4;
 lost_train_ratio = 0.6;
 isl_train_ratio = 0.6;
 
-gram_test_ratio = 0.001;
+gram_test_ratio = -0.1;
 gram_split_test_ratio = 1;
 idot_test_ratio = 1;
 lost_test_ratio = 1;
