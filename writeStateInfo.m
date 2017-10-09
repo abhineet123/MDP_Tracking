b7630bd50ecb80881b3e53afe02ce2f8e3cbdf9b
abhineet123 <asingh1@ualberta.ca>
2017-10-09 10:13:29 -0600

    added pause_for_debug to tracker struct;

--- writeStateInfo.m
+++ writeStateInfo.m
@@ -1,6 +1,7 @@
 function writeStateInfo( tracker, write_to_bin )
 
 fp_fmt = '%.10f';
+fp_dtype = 'float32';
 
 tracker.features(:, 1) = tracker.medFBs;
 tracker.features(:, 2) = tracker.medFBs_left;
@@ -20,30 +21,36 @@ locations(:, 2) = tracker.dres.y;
 locations(:, 3) = tracker.dres.w;
 locations(:, 4) = tracker.dres.h;
 if write_to_bin  
-    fwrite(fopen('log/active_train_features.bin','w'), tracker.factive','float32');
-    fwrite(fopen('log/active_train_labels.bin','w'), tracker.lactive,'float32');
+    fwrite(fopen('log/active_train_features.bin','w'), tracker.factive',fp_dtype);
+    fwrite(fopen('log/active_train_labels.bin','w'), tracker.lactive,fp_dtype);
+    fwrite(fopen('log/lost_train_features.bin','w'), tracker.f_occluded',fp_dtype);
+    fwrite(fopen('log/lost_train_labels.bin','w'), tracker.l_occluded,fp_dtype);
     fwrite(fopen('log/flags.bin','w'), tracker.flags,'uint8');
     fwrite(fopen('log/indices.bin','w'), tracker.indexes - 1,'uint8');
-    fwrite(fopen('log/overlaps.bin','w'), tracker.overlaps,'float32');
-    fwrite(fopen('log/angles.bin','w'), tracker.angles,'float32');
-    fwrite(fopen('log/ratios.bin','w'), tracker.ratios,'float32');
-    fwrite(fopen('log/bb_overlaps.bin','w'), tracker.bb_overlaps,'float32');
-    fwrite(fopen('log/similarity.bin','w'), tracker.nccs,'float32');
-    fwrite(fopen('log/scores.bin','w'), tracker.scores,'float32');
-    fwrite(fopen('log/patterns.bin','w'), tracker.patterns','float32');
-    fwrite(fopen('log/features.bin','w'), tracker.features','float32');
-    fwrite(fopen('log/lk_out.bin','w'), points','float32');    
+    fwrite(fopen('log/overlaps.bin','w'), tracker.overlaps,fp_dtype);
+    fwrite(fopen('log/angles.bin','w'), tracker.angles,fp_dtype);
+    fwrite(fopen('log/ratios.bin','w'), tracker.ratios,fp_dtype);
+    fwrite(fopen('log/bb_overlaps.bin','w'), tracker.bb_overlaps,fp_dtype);
+    fwrite(fopen('log/similarity.bin','w'), tracker.nccs,fp_dtype);
+    fwrite(fopen('log/scores.bin','w'), tracker.scores,fp_dtype);
+    fwrite(fopen('log/patterns.bin','w'), tracker.patterns',fp_dtype);
+    fwrite(fopen('log/features.bin','w'), tracker.features',fp_dtype);
+    fwrite(fopen('log/lk_out.bin','w'), points',fp_dtype);    
     fwrite(fopen('log/roi.bin','w'), roi','uint8');
     fwrite(fopen('log/ids.bin','w'), tracker.dres.id,'uint8');
     fwrite(fopen('log/frame_ids.bin','w'), tracker.dres.fr - 1,'uint8');
     fwrite(fopen('log/states.bin','w'), tracker.dres.state,'uint8');
-    fwrite(fopen('log/locations.bin','w'), locations','float32');
-    fwrite(fopen('log/history_scores.bin','w'), tracker.dres.r,'float32');
+    fwrite(fopen('log/locations.bin','w'), locations',fp_dtype);
+    fwrite(fopen('log/history_scores.bin','w'), tracker.dres.r,fp_dtype);
     fclose('all');
 else
     dlmwrite('log/active_train_features.txt', tracker.factive, 'delimiter', '\t',...
         'precision',fp_fmt);
     dlmwrite('log/active_train_labels.txt', tracker.lactive, 'delimiter', '\t',...
+        'precision',fp_fmt);
+    dlmwrite('log/lost_train_features.txt', tracker.f_occluded, 'delimiter', '\t',...
+        'precision',fp_fmt);
+    dlmwrite('log/lost_train_labels.txt', tracker.l_occluded, 'delimiter', '\t',...
         'precision',fp_fmt);
     dlmwrite('log/flags.txt', tracker.flags, 'delimiter', '\t',...
         'precision','%d');
