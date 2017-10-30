0f2f04eb683ed3ace3554ba9fc665461b4f285b4
abhineet123 <asingh1@ualberta.ca>
2017-10-30 00:32:18 -0600

    minor fixes

--- globals.m
+++ globals.m
@@ -9,12 +9,12 @@ function opt = globals()
 %% Important settings
 
 opt.is_text = 1;  
-opt.write_state_info = 1;
+opt.write_state_info = 0;
 opt.write_thresh = [62, 1];
 opt.write_to_bin = 1;
 opt.verbose_svm = 0;
 
-train_ratio = 0.1;
+train_ratio = 1;
 test_ratio = 1;
 
 opt.train_start_offset = 0;
