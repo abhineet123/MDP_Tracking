9e38c2a9ee1be665da3b20b7b221613ab616cd11
abhineet123 <asingh1@ualberta.ca>
2017-11-05 07:27:30 -0700

    minor fixes

--- globals.m
+++ globals.m
@@ -12,19 +12,19 @@ function opt = globals()
 % def.seq_idx_test = {[10:15, 25:30], [51:60]};
 % def.seq_idx_train = {[1, 2], [3]};
 % def.seq_idx_test = {[1, 2], [3]};
-opt.seq_idx_train = {[71]};
-opt.seq_idx_test = {[71]};
+opt.seq_idx_train = {[69]};
+opt.seq_idx_test = {[69]};
 
-train_ratio = 0.1;
+train_ratio = 1;
 test_ratio = 1;
 opt.train_start_offset = 0;
 
-opt.is_text = 1;  
-opt.write_state_info = 1;
+opt.is_text = 0;  
+opt.write_state_info = 0;
 % opt.write_thresh = [1,1];
 opt.write_thresh = [10,486];
 opt.write_to_bin = 1;
-opt.verbose_svm = 1;
+opt.verbose_svm = 0;
 
 if exist('train_ratio', 'var')
     gram_train_ratio = train_ratio;
